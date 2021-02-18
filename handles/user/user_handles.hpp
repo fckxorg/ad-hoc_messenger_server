da5b9b82445c8837f3af0f58a10eb237d428544c
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2021-02-19 01:17:07 +0300

    boilerplate for /user/find

---
+++ handles/user/user_handles.hpp
@@ -0,0 +1,6 @@
+#include <crow_all.h>
+#include <iostream>
+
+crow::json::wvalue FindUserHandle(crow::json::wvalue user_handle);
+   
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <crow_all.h>
 
+#include <iostream>
+
 int main() {
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello, Mr. Volkov!";    
-    });
+    CROW_ROUTE(app, "/")([]() { return "Get the fuck out of my room, I'm playing minecraft!"; });
+
+    CROW_ROUTE(app, "/user/find")
+        .methods("POST"_method)([](const crow::request& req) {
+            auto request = crow::json::load(req.body);
+
+            if(!request || !request.has("handle")) {
+                return crow::response(400);
+            }
+            
+            std::cout << request["handle"] << std::endl;
+
+
+            crow::json::wvalue response_body;
+            response_body["status"] = "OK";
+            response_body["public_key"] = "key";
+            
+            crow::response response(response_body.dump());
+            return response;
+        });
+    
+    CROW_ROUTE(app, "/user/auth")
+        .methods("GET"_method, "POST"_method)([](const crow::request& req) {
+            auto request = crow::json::load(req.body);    
+            return "OK";
+        });
 
     app.port(5000).multithreaded().run();
 }
