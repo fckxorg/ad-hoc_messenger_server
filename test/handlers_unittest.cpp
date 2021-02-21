54301cc9cd13ad651ef233a9ac43336b1a2bf75d
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2021-02-21 18:26:24 +0300

    finished tests for user_find_handler

--- test/handlers_unittest.cpp
+++ test/handlers_unittest.cpp
@@ -8,8 +8,14 @@
 
 #include "handlers.hpp"
 
+mongocxx::instance instance{}; // intializing database
+//=================================================
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//              UserFindHandler Tests
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//=================================================
+//
 TEST(UserFindHandler, Success) {
-    mongocxx::instance{};
     mongocxx::client client{mongocxx::uri{}};
     mongocxx::database test_db = client["messenger_db"];
     
@@ -17,9 +23,38 @@ TEST(UserFindHandler, Success) {
     test_request.body = "{\"handle\": \"@fckxorg\"}";
     test_request.method = "POST"_method;
 
-    EXPECT_EQ(user_find_handler(test_request, test_db).code, crow::response(200).code);
+
+    crow::response test_response = user_find_handler(test_request, test_db);
+    crow::json::rvalue response_body = crow::json::load(test_response.body);
+
+
+    EXPECT_EQ(test_response.code, 200);
+    EXPECT_EQ(response_body.has("public_key"), true); 
 }
 
+TEST(UserFindHandler, InvalidJson) {
+    
+    mongocxx::client client{mongocxx::uri{}};
+    mongocxx::database test_db = client["messenger_db"];
+    
+    crow::request test_request{};
+    test_request.body = "{\"handle: \"@fckxorg\"}";
+    test_request.method = "POST"_method;
+
+    EXPECT_EQ(user_find_handler(test_request, test_db).code, 400);
+}
+
+TEST(UserFindHandler, UserNotFound) {
+    
+    mongocxx::client client{mongocxx::uri{}};
+    mongocxx::database test_db = client["messenger_db"];
+    
+    crow::request test_request{};
+    test_request.body = "{\"handle\": \"@C0FF33\"}";
+    test_request.method = "POST"_method;
+
+    EXPECT_EQ(user_find_handler(test_request, test_db).code, 404);
+}
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
